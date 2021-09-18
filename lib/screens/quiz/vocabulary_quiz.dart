f058c37081a56a229fd5dbaac2feb6260be6891e
hongyu1738 <70249177+hongyu1738@users.noreply.github.com>
2021-09-18 18:34:50 +0800

    Update vocabulary_quiz.dart
    - Updated error checking for VocabularyQuiz

--- lib/screens/quiz/vocabulary_quiz.dart
+++ lib/screens/quiz/vocabulary_quiz.dart
@@ -88,7 +88,7 @@ class _VocabularyQuizState extends State<VocabularyQuiz> {
               child: Consumer<ImageData>(
                   builder: (context, value, child){
                     return (value.vocabularyImageLabel == '' && value.vocabularyImageUrl == '' && !value.vocabularyError)
-                    && (value.choiceResults.length == 0 && value.answerChoices.length == 0)
+                    //&& (value.choiceResults.length == 0 && value.answerChoices.length == 0)
                     ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator())) 
                     : value.vocabularyError ? SizedBox(
                       height: MediaQuery.of(context).size.height * 0.75,
@@ -109,12 +109,12 @@ class _VocabularyQuizState extends State<VocabularyQuiz> {
                     //   fontSize: 24,
                     //   fontWeight: FontWeight.w400,
                     // ), )
-                    : value.choiceResults.length != 0 && value.answerChoices.length == 0
-                    //Load circular progress indicator when fetching data for answerChocies
-                    ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator()))
-                    : value.choiceResults.length == 0 && value.answerChoices.length != 0
-                    //Load circular progress indicator when fetching data for choiceResults
-                    ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator()))
+                    // : value.choiceResults.length != 0 && value.answerChoices.length == 0
+                    // //Load circular progress indicator when fetching data for answerChocies
+                    // ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator()))
+                    // : value.choiceResults.length == 0 && value.answerChoices.length != 0
+                    // //Load circular progress indicator when fetching data for choiceResults
+                    // ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator()))
                     : value.vocabularyImageLabel == '' && value.vocabularyImageUrl != ''
                     //Load circular progress indicator when fetching data for vocabularyImageLabel
                     ? SizedBox(height: MediaQuery.of(context).size.height, child: Center(child: CircularProgressIndicator()))
